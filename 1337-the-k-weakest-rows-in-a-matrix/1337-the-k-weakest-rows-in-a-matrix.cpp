6ebdec849a87a69ef98cf34bf4c033c7bb828660
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-07-01 13:40:55 +0530

    Time: 1 ms (14.31%), Space: 15 MB (5.62%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    int check(int idx, vector<vector<int>> mat){
-        int count = 0;
-        for(int i=0; i< mat[0].size(); i++){
-          if(mat[idx][i]== 1){
-            count++;
-          }
+    int f(vector<int> soldier){
+        int low= 0;
+        int high= soldier.size() - 1;
+        int res= soldier.size();
+        while(low<= high){
+            int mid = low + (high- low)/2;
+            if(soldier[mid]== 1){
+                low= mid + 1;
+            }
+            else{
+                res= mid;
+                high= mid - 1;
+            }
         }
-        return count;
+        return res;
     }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-       priority_queue<pair<int, int>> pq;
-       for(int i=0; i< k; i++){
-        pq.push({check(i, mat), i});
-       }
-       for(int i=k; i< mat.size(); i++){
-        pair<int, int> curr= {check(i, mat), i};
+     priority_queue<pair<int, int>> pq;
+     for(int i=0; i< k; i++){
+        int sold= f(mat[i]);
+        pq.push({sold, i});
+     } 
+     for(int j=k; j< mat.size(); j++){
+        int sold= f(mat[j]);
+        pair<int, int> curr= {sold, j};
         if(curr< pq.top()){
             pq.pop();
             pq.push(curr);
         }
-       } 
-       vector<int> res;
-       while(!pq.empty()){
+     }
+     vector<int> res;
+     while(!pq.empty()){
         res.push_back(pq.top().second);
         pq.pop();
-       }
-       reverse(res.begin(), res.end());
-       return res;
+     }
+     reverse(res.begin(), res.end());
+     return res;
     }
 };
