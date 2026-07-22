2657f33cc44c2a440e6592251d22042b9eb6cfc0
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-07-22 22:19:49 +0530

    Time: 64 ms (95.44%), Space: 126.1 MB (80.49%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,31 +1,40 @@
-class Solution {
-public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        priority_queue<pair<int, int>> unlock;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> lock;
-        int n= capital.size();
-        for(int i=0; i< n; i++){
-            if(capital[i]<= w){
-                unlock.push({profits[i], capital[i]});
+class Solution
+{
+    public:
+        int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+        {
+            priority_queue<int> profit;
+            priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> capi;
+            int n = profits.size();
+            for (int i = 0; i < n; i++)
+            {
+                if (capital[i] <= w)
+                {
+                    profit.push(profits[i]);
+                }
+                else
+                {
+                    capi.push({ capital[i],
+                        profits[i] });
+                }
             }
-            else{
-                lock.push({capital[i], profits[i]});
+            while (k--)
+            {
+                if (profit.empty())
+                {
+                    return w;
+                }
+                else
+                {
+                    w = w + profit.top();
+                    profit.pop();
+                }
+                while (!capi.empty() and w >= capi.top().first)
+                {
+                    profit.push(capi.top().second);
+                    capi.pop();
+                }
             }
+            return w;
         }
-        while(k>0){
-            if(unlock.empty()){
-                break;
-            }
-            auto temp= unlock.top();
-            w= w + temp.first;
-            unlock.pop();
-            k--;
-            while(!lock.empty() and w>= lock.top().first){
-               auto x= lock.top();
-               unlock.push({x.second, x.first});
-               lock.pop();
-            }
-        }
-        return w;
-    }
 };
