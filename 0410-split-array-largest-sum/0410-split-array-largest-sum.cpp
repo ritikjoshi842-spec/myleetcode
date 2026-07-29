56a98e47d906f3c12765c3954ca0db55e306ec59
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-07-29 11:02:04 +0530

    Time: 0 ms (100%), Space: 11.1 MB (6.35%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-bool check(vector<int> &nums, int k, int guess){
-    int ans = 0;
-    int count = 1;
-    for(int i=0; i< nums.size(); i++){
-        if(ans + nums[i]<= guess){
-            ans= ans + nums[i];
-        }
-        else{
-            ans= nums[i];
-            count++;
-        }
-    }
-    if(count<= k){
-        return true;
-    }
-    return false;
-}
-    int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());
-        int sum = 0;
+    bool fx(int mid, vector<int> nums, int k){
+        int count = 1;
+        int distribute= 0;
         for(int i=0; i< nums.size(); i++){
-            sum = sum + nums[i];
-        }
-        int high= sum;
-        int res= -1;
-        while(low<= high){
-            int guess= low + (high - low)/2;
-            if(check(nums, k, guess)){
-                high= guess - 1;
-                res= guess;
+            if(distribute + nums[i]<= mid){
+                distribute= distribute + nums[i];
             }
             else{
-                low = guess + 1;
+                count++;
+                distribute= nums[i];
+            }
+        }
+        if(count<= k){
+            return true;
+        }
+        return false;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low= *max_element(nums.begin(), nums.end());
+        int high= 0;
+        for(int i=0; i< nums.size(); i++){
+            high= high + nums[i];
+        }
+        int res= 0;
+        while(low<= high){
+            int mid= low + (high - low)/2;
+            if(fx(mid, nums, k)== true){
+                res= mid;
+                high= mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
         return res;
