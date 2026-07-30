11c6864cc07c243e4fd8f760b47db22d00df4480
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-07-30 13:18:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.58%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void fx(vector<string> &res, unordered_map<char, string> &mp, int idx, string &digits,
-    string &temp){
-        if(idx== digits.size()){
-            res.push_back(temp);
-            return;
-        }
-        string output = mp[digits[idx]];
-        for(int i=0; i< output.size(); i++){
-            temp.push_back(output[i]);
-            fx(res, mp, idx+ 1, digits, temp);
-            temp.pop_back();
-        }
+    void fx(string &digits, unordered_map<char, string> &mp, int idx, vector<string> &res, string &temp){
+      if(idx== digits.size()){
+         res.push_back(temp);
+         return;
+      }
+      string letters= mp[digits[idx]];
+      for(int i=0; i< letters.size(); i++){
+        temp.push_back(letters[i]);
+        fx(digits, mp, idx+1, res, temp);
+        temp.pop_back();
+      }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        unordered_map<char, string> mp;
-        string temp;
-        mp['2']= "abc";
-        mp['3']= "def";
-        mp['4']= "ghi";
-        mp['5']= "jkl";
-        mp['6']= "mno";
-        mp['7']= "pqrs";
-        mp['8']= "tuv";
-        mp['9']= "wxyz";
-        fx(res, mp, 0, digits, temp);
-        return res;
+       unordered_map<char, string> mp;
+       mp['2']= "abc";
+       mp['3']= "def";
+       mp['4']= "ghi";
+       mp['5']= "jkl";
+       mp['6']= "mno";
+       mp['7']= "pqrs";
+       mp['8']= "tuv";
+       mp['9']= "wxyz";
+       vector<string> res;
+       string temp;
+       fx(digits, mp, 0, res, temp); 
+       return res;
     }
 };
