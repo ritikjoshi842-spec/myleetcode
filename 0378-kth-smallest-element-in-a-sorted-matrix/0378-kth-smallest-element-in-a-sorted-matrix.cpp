7c5e3783c1b4bb9dbeaae98ec634a94c161e3f1b
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-08-18 20:08:28 +0530

    Time: 1 ms (63.39%), Space: 17.1 MB (79.96%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &matrix, int k, int target){
+    int check(vector<vector<int>> &matrix, int k, int guess){
         int rows= matrix.size();
-        int cols= matrix.size();
-        int row = rows- 1;
-        int col= 0;
-        int count= 0;
+        int cols = matrix.size();
+        int row = rows - 1;
+        int col = 0;
+        int count = 0;
         while(row>= 0 and col< cols){
-            if(matrix[row][col]<= target){
-                count= count + row + 1;
-                col++;
+            if(matrix[row][col]<= guess){
+               count = count + row + 1;
+               col++; 
             }
-            else if(matrix[row][col]> target){
+            else{
                 row--;
             }
         }
-        return count;
+        return count; 
     }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int low = matrix[0][0];
-        int n= matrix.size()- 1;
-        int high = matrix[n][n];
-        int res= 0;
-        while(low<= high){
-            int mid = low + (high - low)/2;
-            int count = solve(matrix, k, mid);
-            if(count>= k){
-                res= mid;
-                high= mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+       int n = matrix.size() - 1;
+       int low = matrix[0][0];
+       int high = matrix[n][n];
+       int res = 0;
+       while(low<= high){
+        int guess = low + (high - low)/2;
+        if(check(matrix, k, guess)>= k){
+           res= guess;
+           high = guess - 1;
+        }   
+        else{
+           low = guess + 1;
         }
-        return res;
+       }
+       return res;
     }
 };
