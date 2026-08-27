de5dfd639ecaf4c5e24516633524cadfdfc903fc
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-08-27 11:49:31 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.2%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-     if(nums1.size()> nums2.size()){
+       if(nums1.size()> nums2.size()){
         return findMedianSortedArrays(nums2, nums1);
-     }
-     int low1 = 0;
-     int n1 = nums1.size();
-     int n2 = nums2.size();
-     int low2= 0;
-     int high1 = n1;
-     int high2= n2;
-     int before_mid1, before_mid2, after_mid1, after_mid2;
-     while(low1<= high1){
-        int mid1 = low1 + (high1 -low1)/2;
-        int mid2 = (n1 + n2 + 1)/2 - mid1;
-        if(mid1== 0){
+       } 
+       int n1 = nums1.size();
+       int n2 = nums2.size();
+       int low1 = 0;
+       int low2 = 0;
+       int high1 = nums1.size();
+       int high2 = nums2.size();
+       int before_mid1, before_mid2, after_mid1, after_mid2;
+       while(low1<= high1){
+        int mid1 = low1 + (high1 - low1)/2;
+        int mid2 = ((n1 + n2 + 1)/2) - mid1;
+        if(mid1==0){
             before_mid1 = INT_MIN;
         }
         else{
             before_mid1 = nums1[mid1- 1];
         }
         if(mid2== 0){
-            before_mid2= INT_MIN;
+            before_mid2 = INT_MIN;
         }
         else{
-            before_mid2= nums2[mid2- 1];
+            before_mid2 = nums2[mid2 - 1];
         }
         if(mid1== n1){
-            after_mid1= INT_MAX;
+            after_mid1 = INT_MAX;
         }
         else{
-            after_mid1= nums1[mid1];
+            after_mid1 = nums1[mid1];
         }
         if(mid2== n2){
-            after_mid2= INT_MAX;
+            after_mid2 = INT_MAX;
         }
         else{
             after_mid2= nums2[mid2];
         }
         if(before_mid1<= after_mid2 and before_mid2<= after_mid1){
             if((n1 + n2)%2== 0){
-                double median1 = max(before_mid1, before_mid2);
-                double median2 = min(after_mid1, after_mid2);
-                return (median1 + median2)/2.0;
+              double x= max(before_mid1, before_mid2);
+              double y = min(after_mid1, after_mid2);
+              return (double)((x + y)/2.0);
             }
             else{
                 return max(before_mid1, before_mid2);
             }
         }
         else if(before_mid1> after_mid2){
-               high1 = mid1 - 1;
+            high1 = mid1 - 1;
         }
         else{
             low1 = mid1 + 1;
         }
-     }
-     return 0;   
+       } 
+       return -1; 
     }
 };
