3aab645eb6e4eef04e6b636f0a793dfd78831c00
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-08-29 13:30:10 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.11%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-    int f(vector<int> soldier){
-        int low= 0;
-        int high= soldier.size() - 1;
-        int res= soldier.size();
+    int get_weak(vector<int> &temp){
+        int low = 0;
+        int high = temp.size()- 1;
+        int ans = temp.size(); 
         while(low<= high){
-            int mid = low + (high- low)/2;
-            if(soldier[mid]== 1){
-                low= mid + 1;
-            }
-            else{
-                res= mid;
-                high= mid - 1;
-            }
+          int mid = low + (high - low)/2;
+          if(temp[mid]== 0){
+            ans = mid;
+            high = mid - 1;
+          }
+          else{
+            low = mid + 1;
+          }
         }
-        return res;
+        return ans;
     }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-     priority_queue<pair<int, int>> pq;
-     for(int i=0; i< k; i++){
-        int sold= f(mat[i]);
-        pq.push({sold, i});
-     } 
-     for(int j=k; j< mat.size(); j++){
-        int sold= f(mat[j]);
-        pair<int, int> curr= {sold, j};
-        if(curr< pq.top()){
-            pq.pop();
-            pq.push(curr);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int n = mat.size();
+        for(int i = 0; i< n; i++){
+            int x= get_weak(mat[i]);
+            pq.push({x, i});
         }
-     }
-     vector<int> res;
-     while(!pq.empty()){
-        res.push_back(pq.top().second);
-        pq.pop();
-     }
-     reverse(res.begin(), res.end());
-     return res;
-    }
+        vector<int> res;
+        while(k--){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    } 
 };
