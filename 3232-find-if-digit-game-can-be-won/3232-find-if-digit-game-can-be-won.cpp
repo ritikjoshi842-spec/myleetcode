32568d77a5980511b2f4eeeee3d302b626263b9f
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-09-08 23:06:27 +0530

    Time: 0 ms (100%), Space: 28.7 MB (11.23%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i]/10 == 0){
+                sum1 = sum1 + nums[i];
+            }
+            else{
+                sum2 = sum2 + nums[i];
+            }
+        }
+        if(sum1== sum2){
+            return false;
+        }
+        return true;
+    }
+};
