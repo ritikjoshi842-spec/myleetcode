7c7451b234be1c901a970569483fb6edbae5a875
ritikjoshi842-spec <ritikjoshi842@gmail.com>
2026-09-11 20:19:05 +0530

    Time: 2 ms (29.01%), Space: 28.9 MB (71.64%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,33 +11,28 @@
  */
 class Solution {
 public:
-bool fx(TreeNode *root, TreeNode *subRoot){
-    if(root== nullptr and subRoot== nullptr){
-        return true;
+bool res= false;
+    bool fx(TreeNode* root, TreeNode* subRoot){
+        if(root== nullptr and subRoot== nullptr){
+            return true;
+        }
+        if(root== nullptr or subRoot== nullptr){
+            return false;
+        }
+        bool left = fx(root-> left, subRoot-> left);
+        bool right = fx(root-> right, subRoot-> right);
+        return left && right && root-> val == subRoot-> val;
     }
-    if(root== nullptr or subRoot== nullptr){
-        return false;
-    }
-    if(root-> val != subRoot-> val){
-        return false;
-    }
-    bool v1= fx(root-> left, subRoot-> left);
-    bool v2= fx(root-> right, subRoot-> right);
-    if(v1== true and v2== true){
-        return true;
-    }
-    return false;
-}
-    bool f(TreeNode* root, TreeNode *subRoot){
-        if(root== nullptr){
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root== nullptr and subRoot== nullptr){
+            return true;
+        }
+        if(root== nullptr or subRoot== nullptr){
             return false;
         }
         if(root-> val == subRoot-> val and fx(root, subRoot)){
             return true;
         }
-        return f(root-> left, subRoot) or f(root-> right, subRoot);
-    }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        return f(root, subRoot);
+        return isSubtree(root-> left, subRoot) or isSubtree(root-> right, subRoot);
     }
 };
